5402a6a3145fd2f2e8958712d0c07d34d8c2ba9a
KnightCodder <knightcodder@gmail.com>
2026-03-02 16:56:46 +0530

    standard register name

--- SimpleAssembler/toBinary.py
+++ SimpleAssembler/toBinary.py
@@ -51,6 +51,52 @@ INSTRUCTIONtoOPCODE = {
     "jal": "1101111",
 }
 
+ABItoX = {
+    "zero": "x0",
+    "ra": "x1",
+    "sp": "x2",
+    "gp": "x3",
+    "tp": "x4",
+    "t0": "x5",
+    "t1": "x6",
+    "t2": "x7",
+    "s0": "x8",
+    "fp": "x8",
+    "s1": "x9",
+    "a0": "x10",
+    "a1": "x11",
+    "a2": "x12",
+    "a3": "x13",
+    "a4": "x14",
+    "a5": "x15",
+    "a6": "x16",
+    "a7": "x17",
+    "s2": "x18",
+    "s3": "x19",
+    "s4": "x20",
+    "s5": "x21",
+    "s6": "x22",
+    "s7": "x23",
+    "s8": "x24",
+    "s9": "x25",
+    "s10": "x26",
+    "s11": "x27",
+    "t3": "x28",
+    "t4": "x29",
+    "t5": "x30",
+    "t5": "x31",
+}
+
+def standard_line(words):
+    new_words = []
+    for word in words:
+        if word in ABItoX.keys():
+            new_words.append(ABItoX[word])
+        else:
+            new_words.append(word)
+    return new_words
+        
+
 def convertToBinary(lines):
     for i, line in enumerate(lines):
         words = line.split()
@@ -61,8 +107,11 @@ def convertToBinary(lines):
             print("error in line", i+1)
             return
         
+        words = standard_line(words)
+
         opcode = INSTRUCTIONtoOPCODE[words[0]]
         type = OPCODEStoINSTRUCTIONTYPE[opcode]
+
 
         binary = ""
 
