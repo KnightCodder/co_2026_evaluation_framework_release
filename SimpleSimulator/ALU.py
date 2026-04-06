985972406d07a351207361529dbc9fa3de60b6ae
Divyansh1080 <renu.paharia83@gmail.com>
2026-04-06 12:30:01 +0530

    did ALU

--- SimpleSimulator/ALU.py
+++ SimpleSimulator/ALU.py
@@ -1,4 +1,26 @@
 from error import SimulatorError
 
 def ALU(SrcA: int, SrcB: int, ALUControl: int):
-    return 0
+    mask=0xFFFFFFFF
+    if ALUControl==0b0010:  
+        return (SrcA+SrcB) &mask
+    elif ALUControl==0b0110:  
+        return (SrcA-SrcB) &mask
+    elif ALUControl==0b0011: 
+        return (SrcA<<(SrcB&0x1F)) &mask 
+    elif ALUControl==0b0100: 
+        a_sign=(SrcA & mask)-0x100000000 if(SrcA & 0x80000000) else(SrcA & mask)
+        b_sign=(SrcB & mask)-0x100000000 if(SrcB & 0x80000000) else(SrcB & mask)
+        return 1 if a_sign<b_sign else 0
+    elif ALUControl==0b0101: 
+        return 1 if (SrcA&mask)<(SrcB&mask) else 0
+    elif ALUControl==0b0111: 
+        return SrcA^SrcB
+    elif ALUControl==0b1000: 
+        return (SrcA&mask)>>(SrcB&0x1F)
+    elif ALUControl==0b0001: 
+        return SrcA|SrcB
+    elif ALUControl==0b0000: 
+        return SrcA&SrcB
+    else:
+        raise SimulatorError("invalid ALU control")
