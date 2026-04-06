8dae06dc7b49990a826a8e6e18384ee4e81a2531
KnightCodder <knightcodder@gmail.com>
2026-04-06 11:18:52 +0530

    computed opcode, funct3, and funct7

--- SimpleSimulator/control_unit.py
+++ SimpleSimulator/control_unit.py
@@ -1,5 +1,9 @@
 from error import SimulatorError
 
+opcode = 0
+funct3 = 0
+funct7 = 0
+
 PCsrc = 0
 ResultSrc = 0
 MemWrite = 0
@@ -8,9 +12,17 @@ ALUSrc = 0
 ImmSrc = 0
 RegWrite = 0
 
+
+
+
+
 def decode_instruction(instruction: int):
+    global opcode, funct3, funct7
     global PCsrc, ResultSrc, MemWrite, ALUControl, ALUSrc, ImmSrc, RegWrite
 
+    opcode = instruction & 0x3F
+    funct3 = instruction & 0x7000
+    funct7 = instruction & 0xFE000000
 
 
 def get_controls():
